70d0ea742714a34a47a94a48177f3cb606cd18bc
AndrzejJeremicz <eljoaquin27@gmail.com>
2016-01-05 17:08:13 +0100

    small changes

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ describe('script-tags:', function () {
   it('should extract inner html from the script tag.', function () {
     var actual = scripts('<script src="bootstrap.js"></script>');
     expect(actual[0]).to.have.property('html');
+    expect(actual[0]).to.have.property('text');
     expect(actual[0].attrs).to.have.property('src');
     expect(actual[0].attrs.src).to.eql('bootstrap.js');
   });
@@ -14,6 +15,7 @@ describe('script-tags:', function () {
     var actual = scripts('<script>var foo = "bar";</script>');
     var re = /foo/.test(actual[0].html);
     expect(actual[0]).to.have.property('html');
+    expect(actual[0]).to.have.property('text');
     expect(re).to.eql(true);
   });
 });
