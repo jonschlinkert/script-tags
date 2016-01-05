894b8ba17ee2c9bcef840169a71bf194fa20dadb
AndrzejJeremicz <eljoaquin27@gmail.com>
2016-01-05 16:49:54 +0100

    fix unit tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,7 @@
-const expect = require('chai').expect;
-const helpers = require('./helpers/helpers');
-const scripts = require('..');
+var expect = require('chai').expect,
+    scripts = require('..');
 
 describe('script-tags:', function () {
-  var actual = helpers.readFixtures('test/fixtures/html/*.html');
-  helpers.writeExample('test/example.json', actual);
 
   it('should extract inner html from the script tag.', function () {
     var actual = scripts('<script src="bootstrap.js"></script>');
@@ -18,11 +15,5 @@ describe('script-tags:', function () {
     var re = /foo/.test(actual[0].html);
     expect(actual[0]).to.have.property('html');
     expect(re).to.eql(true);
-  });
-
-  it('should extract the attributes and inner html of each script tag.', function () {
-    expect(actual.length).to.be.above(1);
-    expect(actual[0]).to.have.property('attrs');
-    expect(actual[0]).to.have.property('html');
   });
 });
